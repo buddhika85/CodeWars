4bd570c4dba123431678c2992c2f9f5105271f56
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-13 18:57:51 +0530

    work in progress

---
+++ CodeChallenges/PokerHand.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeChallenges;
+
+// https://www.codewars.com/kata/5ae64f28d2ee274164000118/train/csharp
+
+public class JomoPipi
+{
+    public static string StringFunc(string s, long x)
+    {
+        //code here
+        return "";
+    }
+}
