a4ec904d4c88f3c627e2e42cf1ff897de5256f5c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-13 22:46:15 +0530

    code times out for some reason

--- CodeChallenges/PokerHand.cs
+++ CodeChallenges/PokerHand.cs
@@ -1,12 +1,61 @@
-﻿namespace CodeChallenges;
+﻿using System.Text;
+
+namespace CodeChallenges;
 
 // https://www.codewars.com/kata/5ae64f28d2ee274164000118/train/csharp
 
 public class JomoPipi
 {
-    public static string StringFunc(string s, long x)
+    public string StringFunc(string str, long times)
     {
-        //code here
-        return "";
+        Console.WriteLine($"Original : {str}");
+        var chars = str.ToCharArray();
+        var count = 1;
+        while (count <= times)
+        {
+            chars = Reversal(chars);
+            //Console.WriteLine($"{count} : {string.Join(null, chars)}");
+            ++count;
+        }
+        //str = string.Join(null, chars);
+        //Console.WriteLine($"Result : {str}");
+        //return str;
+        StringBuilder sb = new();
+        foreach (var item in chars)
+        {
+            sb.Append(item);
+        }
+        return sb.ToString();
+    }
+
+
+    private char[] Reversal(char[] chars)
+    {
+        chars = chars.Reverse().ToArray();
+        //Console.WriteLine($"Reverse : {string.Join(null, chars)}");
+
+        var round = 1;
+
+        while (round < chars.Length - 1)
+        {
+            var start = round;
+            var end = chars.Length - 1;
+
+            while (start < end)
+            {
+                (chars[start], chars[end]) = (chars[end], chars[start]);
+                /*
+                var temp = chars[start];
+                chars[start] = chars[end];
+                chars[end] = temp;
+                 */
+                ++start;
+                --end;
+            }
+
+            //Console.WriteLine($"{round} : {string.Join(null, chars)}");
+            round++;
+        }
+        return chars;
     }
 }
--- LinqPadTests/1.linq
+++ LinqPadTests/1.linq
@@ -6,7 +6,7 @@ void Main()
 {
 	Console.WriteLine($"{Test("String", 3) == "nrtgSi"}{Environment.NewLine}"); 	
 	Console.WriteLine($"{Test("This is a string exemplification!", 0) == "This is a string exemplification!"}{Environment.NewLine}");
-	Console.WriteLine($"{Test("Ohh Man God Damn", 7) == "nGOnmohaadhMD"}{Environment.NewLine}");
+	Console.WriteLine($"{Test("Ohh Man God Damn", 7) == " nGOnmohaadhMD  "}{Environment.NewLine}");
 	Console.WriteLine($"{Test("Ohh Man God Damnn", 19) == "haG mnad MhO noDn"}{Environment.NewLine}");
 }
 
@@ -15,15 +15,21 @@ private string Test(string str, int times)
 	Console.WriteLine($"Original : {str}");
 	var chars = str.ToCharArray();
 	int count = 1;
-	while (count <= times)
+	long iterationsRequired =  times % calculateCycleLength(str.Length);
+	while (count <= iterationsRequired)
 	{
 		chars = Reversal(chars);
 		//Console.WriteLine($"{count} : {string.Join(null, chars)}");
 		++count;
 	}
-	str = string.Join(null, chars);
-	Console.WriteLine($"Result : {str}");
-	return str;
+	//str = string.Join(null, chars);
+	StringBuilder sb = new();
+	foreach (var item in chars)
+	{
+		sb.Append(item);
+	}
+	Console.WriteLine($"Result : {sb.ToString()}");
+	return sb.ToString();
 }
 
 private char[] Reversal(char[] chars)
@@ -51,4 +57,23 @@ private char[] Reversal(char[] chars)
 		round++;
 	}	
 	return chars;
+}
+
+
+private static int calculateCycleLength(int n)
+{
+	// Cache these so we only calculate them once.
+	int a = 2 * n + 1;
+	int b = 2 * n;
+
+	int cycleLength = 1;
+	while (true)
+	{
+		double c = Math.Pow(2, cycleLength);
+		if (c % a == 1 || c % a == b)
+		{
+			return cycleLength;
+		}
+		cycleLength++;
+	}
 }
