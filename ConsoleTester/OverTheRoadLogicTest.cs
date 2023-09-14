c9e7defaa6d2019dc2452f61fa96c5885cb72c63
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-14 13:02:22 +0530

    kata - over the road - console tests

---
+++ ConsoleTester/OverTheRoadLogicTest.cs
@@ -0,0 +1,16 @@
+﻿using CodeChallenges;
+
+namespace ConsoleTester;
+
+using static System.Console;
+public class OverTheRoadLogicTest
+{
+    public void Test()
+    {
+        WriteLine($"{OverTheRoadLogic.OverTheRoad(1, 3) == 6}");
+        WriteLine($"{OverTheRoadLogic.OverTheRoad(3, 3) == 4}");
+        WriteLine($"{OverTheRoadLogic.OverTheRoad(2, 3) == 5}");
+        WriteLine($"{OverTheRoadLogic.OverTheRoad(3, 5) == 8}");
+        WriteLine($"{OverTheRoadLogic.OverTheRoad(7, 11) == 16}");
+    }
+}
---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,4 @@
+﻿
+using ConsoleTester;
+
+new OverTheRoadLogicTest().Test();
